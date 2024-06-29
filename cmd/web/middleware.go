aa8c31babbffb67d5fad36e4c2c88f8b992ae48c
Dev Keshwani <dev.keshwani345@gmail.com>
2024-06-29 18:55:48 +0530

    feat: added security headers using middlewares

---
+++ cmd/web/middleware.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+)
+
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// CSP
+		w.Header().Set("Content-Security-Policy","default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		//refer policy
+		w.Header().Set("Referrer-Policy", "same-origin")
+		//X-Frame-Options
+		w.Header().Set("X-Frame-Options", "deny")
+		//X-XSS-Protection
+		w.Header().Set("X-XSS-Protection","0")
+		//X-Content-Type-Options
+		w.Header().Set("X-Content-Type-Options","nosniff")
+
+		next.ServeHTTP(w, r)
+	})
+}
