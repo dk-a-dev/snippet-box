31faaccd31a8f12ab48feb988b9ae794262f76da
Dev Keshwani <dev.keshwani345@gmail.com>
2024-06-29 19:11:16 +0530

    feat: added logger middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -7,16 +7,23 @@ import (
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// CSP
-		w.Header().Set("Content-Security-Policy","default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 		//refer policy
 		w.Header().Set("Referrer-Policy", "same-origin")
 		//X-Frame-Options
 		w.Header().Set("X-Frame-Options", "deny")
 		//X-XSS-Protection
-		w.Header().Set("X-XSS-Protection","0")
+		w.Header().Set("X-XSS-Protection", "0")
 		//X-Content-Type-Options
-		w.Header().Set("X-Content-Type-Options","nosniff")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
 
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 		next.ServeHTTP(w, r)
 	})
 }
