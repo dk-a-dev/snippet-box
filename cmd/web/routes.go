396c19ad8e0cd992046e6a63fcfd416b204669a6
Dev Keshwani <dev.keshwani345@gmail.com>
2024-06-30 08:41:55 +0530

    feat: mux to httprouter, routing improved

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,23 +1,30 @@
 package main
 
 import (
+	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 	"net/http"
 	"path/filepath"
-	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+
+	router := httprouter.New()
 
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
-	mux.Handle("/static", http.NotFoundHandler())
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	return standard.Then(mux)
+	return standard.Then(router)
 	// return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
 
