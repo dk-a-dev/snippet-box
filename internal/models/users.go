3e77ce6db00232199380da1bd5f71ff2810057d6
Dev Keshwani <dev.keshwani345@gmail.com>
2024-07-03 10:00:06 +0530

    fix: improved authorization logic+refactor

--- internal/models/users.go
+++ internal/models/users.go
@@ -63,6 +63,13 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 			return 0, err
 		}
 	}
-	
+
 	return id, nil
+}
+
+func (m *UserModel) Exists(id int) (bool, error) {
+	var exists bool
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
